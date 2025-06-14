194998b7173d7e803373bb67f242f8a67003ed3e
oKerne <o054841576@gmail.com>
2025-06-14 23:47:39 +0300

    remove double slash from base URL

--- src/app/models/AuthInterceptor .ts
+++ src/app/models/AuthInterceptor .ts
@@ -1,15 +1,3 @@
-// import { HttpInterceptorFn } from '@angular/common/http';
-
-// const BASE_URL = 'https://courseonlineserver.onrender.com';
-
-// export const baseUrlInterceptor: HttpInterceptorFn = (req, next) => {
- 
-//   if (!req.url.startsWith('http')) {
-//     const apiReq = req.clone({ url: `${BASE_URL}${req.url}` });
-//     return next(apiReq);
-//   }
-//   return next(req);
-// };
 import { HttpInterceptorFn } from '@angular/common/http';
 
 const BASE_URL = 'https://courseonlineserver.onrender.com';
